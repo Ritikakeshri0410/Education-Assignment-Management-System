084e659c8083f6372fb7323db821c55fe2a18c39
Ritikakeshri0410 <chwturitika@gmail.com>
2018-06-30 11:56:29 -0700

    In progree database schema design

---
+++ database-schema.py
@@ -0,0 +1,32 @@
+"""
+Table Name
+
+User
+User_id	        number
+User_name       varchar
+first_name      varchar
+last_name       varchar
+Phone_number    number
+Address         varchar
+Email_id        varchar
+User_type       varchar
+Branch          varchar
+
+
+Assignment
+assignment_id
+defination
+description
+submission_date
+start_date
+assignment_score
+cut_off_score
+secured_score
+user_id
+created_by
+created_on
+updated_by
+updated_on
+reviewed_by
+reviewed_on
+"""
